c4586719dd3a26061531b9743d467018edf33274
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2023-12-14 18:48:55 -0500

    created basic structure/wireframe

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <main>
+      <h1>Bird Santuary</h1>
+      <div className='receipt'>
+        Receipt
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <aside>
+        <form className="form">
+        Donation Form 
+        </form>
+      </aside>
+        <div>
+          <table className='table'>
+            <span>Bird Card</span>
+            <br />
+            <span>Bird Card</span>
+            <br />
+            <span>Bird Card</span>
+          </table>
+        </div>
+      </main>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
